ba2d35298d10f803824eafaf6bce7eb42fbb6805
Martin Correa <martin.correa@alkemy.org>
2022-09-09 14:44:57 -0300

    minor change

--- src/com/alkemy/Grupo2/Main.java
+++ src/com/alkemy/Grupo2/Main.java
@@ -3,6 +3,6 @@ package com.alkemy.Grupo2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Grupo 2!");
     }
 }
